74449210e44692584a9510fed6b6ea1480ab3c89
André Marks <andrem4rks@gmail.com>
2023-04-03 15:25:41 -0500

    cria entidade usuario

---
+++ src/main/java/com/br/marks/api/domain/User.java
@@ -0,0 +1,22 @@
+package com.br.marks.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  private String name;
+
+  @Column(unique = true)
+  private String email;
+  private String password;
+}
+
