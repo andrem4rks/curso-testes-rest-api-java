d87c74d9f1a6a5292074e0a1e57ac255453f6857
André Marks <andrem4rks@gmail.com>
2023-04-03 16:43:53 -0500

    trata excecao para objeto nao encontrado

--- src/main/java/com/br/marks/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/br/marks/api/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.br.marks.api.service.impl;
 import com.br.marks.api.domain.User;
 import com.br.marks.api.repository.UserRepository;
 import com.br.marks.api.service.UserService;
+import com.br.marks.api.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class UserServiceImpl implements UserService {
   @Override
   public User findById(Integer id){
     Optional<User> obj = userRepository.findById(id);
-    return obj.orElse(null);
+    return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
   }
 
 }
