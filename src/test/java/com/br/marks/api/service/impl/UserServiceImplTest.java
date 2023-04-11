e62d9c264a1e15d4a456c6c384b629bf14d12063
André Marks <andrem4rks@gmail.com>
2023-04-10 22:37:53 -0500

    cria teste para o metodo findAll da classe UserServiceImpl

--- src/test/java/com/br/marks/api/service/impl/UserServiceImplTest.java
+++ src/test/java/com/br/marks/api/service/impl/UserServiceImplTest.java
@@ -13,6 +13,7 @@ import org.mockito.Spy;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +27,7 @@ class UserServiceImplTest {
     public static final String NAME     = "Valdir";
     public static final String EMAIL    = "valdir@gmail.com";
     public static final String PASSWORD = "123";
+    public static final int INDEX = 0;
 
     @Spy
     @InjectMocks
@@ -75,7 +77,21 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        verify(service, times(1)).findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
